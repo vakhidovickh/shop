10a772bf6234914fa9e9f6f295c0d1168550e71d
Humoyun Mirzo <99760641+vakhidovickh@users.noreply.github.com>
2022-07-05 16:32:56 +0500

    ok

---
+++ shopping/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Nav from '../src/pages/Navbar'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Nav/>
+            </div>
+        );
+    }
+}
+
+export default App;
