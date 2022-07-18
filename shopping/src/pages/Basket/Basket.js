b5f55bc2c85c2edadd70427c5fe277c01864e357
Humoyun Mirzo <99760641+vakhidovickh@users.noreply.github.com>
2022-07-18 20:01:37 +0500

    nnmnmmnmnmn

--- shopping/src/pages/Basket/Basket.js
+++ shopping/src/pages/Basket/Basket.js
@@ -10,11 +10,23 @@ import '../../styles/Basket/Basket.css'
 
 class Basket extends Component {
 
+
+        add=()=>{
+
+        };
+
+    state={
+      shop: localStorage.getItem('bir')
+    };
+
+
+
     render() {
         return (
             <>
                 <NavigationBar/>
                 <Header/>
+                {this.state.shop===null?(
                     <div className="Container">
                         <a href="http://localhost:3000/" className="Back d-flex align-items-center gap-3">
                         <span className="Arrow__left align-items-center">
@@ -29,7 +41,13 @@ class Basket extends Component {
                             <h3 className="fs-2">В вашей корзине пусто? Не беда!</h3>
                             <p className="fs-3 text-gray">Начните выбирать товары из широкого ассортимента нашего каталога.</p>
                         </div>
-                    </div>
+                    </div>):(
+                    this.state.shop.map(item=> {
+return <h1>{item}</h1>
+                    })
+                )
+
+                }
                 <PopularBrands/>
                 <SectionBrands/>
                 <Footer/>
--- shopping/src/pages/News Page/SectionNews.js
+++ shopping/src/pages/News Page/SectionNews.js
@@ -8,40 +8,20 @@ import Footer from "../MainPage/Footer";
 import { FiArrowLeft } from 'react-icons/fi'
 
 class SectionNews extends Component {
-
-    componentDidMount() {
-        for (let i = 0; i < 20; i++) {
-            document.querySelector('.Cards').innerHTML += `
-                                        <div class="Card mb-3">
-                                <div class="Card__header">
-                                    <img src="https://photobuy3.netlify.app/Camera1.png" alt=""/>
-                                </div>
-                                <div class="Card__body">
-                                    <h3>Cовместимость со вспышками — AD200/AD200Pro</h3>
-                                    <p class="Real__price">67 763,23 сум</p>
-                                    <h2>62 392,23 Сум</h2>
-                                </div>
-                                <div class="Card__footer">
-                                    <select name="number" id="number">
-                                        <option value="">1 шт.</option>
-                                        <option value="">2 шт.</option>
-                                        <option value="">3 шт.</option>
-                                        <option value="">4 шт.</option>
-                                        <option value="">5 шт.</option>
-                                        <option value="">6 шт.</option>
-                                    </select>
-                                    <img src="https://photobuy3.netlify.app/cart.png" alt=""/>
-                                </div>
-                            </div>
-            `
-        }
-
-    }
-
+state={
+    data:[],
+    ab:["abbas1","abbas2","abbas3","abbas4"]
+};
+getShop=(item)=>{
+    // this.state.data=localStorage.getItem('bir')
+this.state.data.push(item);
+    localStorage.setItem('bir',this.state.data);
+    console.log(localStorage.getItem('bir'))
+};
     my = () => {
-        document.querySelector('.Cards').innerHTML += ``;
+        document.querySelector('.Cards').innerHTML = ` `;
 
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < 40; i++) {
             document.querySelector('.Cards').innerHTML += `
                                     <div class="Card mb-3">
                                 <div class="Card__header">
@@ -71,6 +51,8 @@ class SectionNews extends Component {
 
 
 
+
+
     render() {
         return (
             <>
@@ -84,7 +66,34 @@ class SectionNews extends Component {
                         <h3>Новинки</h3>
                     </a>
                     <div className="Cards">
+                        {this.state.ab.map(item=>{
+                            return <div className="Card mb-3">
+                                <div className="Card__header">
+                                    <img src="https://photobuy3.netlify.app/Camera1.png" alt=""/>
+                                </div>
+                                <div className="Card__body">
+                                    <h3>Cовместимость со вспышками — AD200/AD200Pro</h3>
+                                    <p className="Real__price">67 763,23 сум</p>
+                                    <h2>62 392,23 Сум</h2>
+                                </div>
+                                <div className="Card__footer">
+                                    <select name="number" id="number">
+                                        <option value="">1 шт.</option>
+                                        <option value="">2 шт.</option>
+                                        <option value="">3 шт.</option>
+                                        <option value="">4 шт.</option>
+                                        <option value="">5 шт.</option>
+                                        <option value="">6 шт.</option>
+                                    </select>
 
+                                    <button className="Btn-add" onClick={()=>this.getShop(item)}>
+
+                                        <img src="https://photobuy3.netlify.app/cart.png" alt=""/>
+                                    </button>
+
+                                </div>
+                            </div>
+                        })}
                     </div>
                     <div className="Button">
                         <button onClick={this.my} className="btn btn-danger mt-5">See More</button>
